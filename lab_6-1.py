85dbcee3c37a7f4873b625e58957723589b258ea
p22cobrien <p22cobrien@fairfieldprep.org>
2021-09-21 10:04:43 -0400

    add hw_6-1.py

---
+++ lab_6-1.py
@@ -0,0 +1,9 @@
+# Author: CMOB 9/21/2021
+
+first_name = input("what is your first name? ")
+
+last_name = input("what is your last name? ")
+
+full_name = first_name + " " + last_name
+
+print(full_name)
